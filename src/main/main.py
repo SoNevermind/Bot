cb9b72e9241aa51217e39881676c6e0a9af211c0
Nevermind <1276411@bsu.edu.ru>
2021-04-08 14:20:49 +0300

    О БОЖЕ ЭТО ЧТО БОТ НЕ МОЖЕТ БЫТЬ!!!!!!!

---
+++ src/main/main.py
@@ -0,0 +1,17 @@
+import telebot
+
+bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот-гид по Солнечной системе. Приятно познакомиться, {message.from_user.first_name}, и да прибудет с тобой сила!')
+
+@bot.message_hendler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Извини, но это я не понимаю')
+
+bot.polling(non_stope=True)
+
