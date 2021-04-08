02413497a784fc242928f6261b882bca58e1e11a
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 17:46:55 +0300

    vgyuvtgmuv

--- src/main/main.py
+++ src/main/main.py
@@ -1,11 +1,4 @@
 import telebot
-from aiogram import Bot, types
-from aiogram.utils import executor
-from aiogram.utils.markdown import text
-from aiogram.dispatcher import Dispatcher
-
-from config import TOKEN
-import button as kb
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
 
@@ -26,12 +19,6 @@ def send_text(message):
         bot.send_message(message.from_user.id, "Напиши Привет")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-
-
-@dp.callback_query_handler(func=lambda c: c.data == 'button1')
-async def process_callback_button1(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, 'Нажата первая кнопка!')
 
 
 bot.polling(none_stop=True, interval=0)
