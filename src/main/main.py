882b97afb71448e35399640cd4ba5f3074b7cde8
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 15:11:58 +0300

    b

--- src/main/main.py
+++ src/main/main.py
@@ -3,19 +3,16 @@ import telebot
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message,
                  f'Я бот-гид по Солнечной системе. Приятно познакомиться, {message.from_user.first_name}, и да '
                  f'прибудет с тобой сила!')
 
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['Привет'])
 def send_text(message):
-    if message.text == 'Привет':
-        bot.send_message(message.chat.id, 'Привет, это гид по Солнечной системе!')
-    elif message.text == 'Пока':
-        bot.send_message(message.chat.id, 'Прощай!')
+    bot.send_message(message.chat.id, 'Привет, вот что я могу!')
 
 
 bot.polling(none_stop=True, interval=0)
