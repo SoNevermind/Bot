8c74f6c6437c09f8b25b0f931ace6d362ac769a5
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 15:40:32 +0300

    xfggf

--- src/main/main.py
+++ src/main/main.py
@@ -1,6 +1,8 @@
 import telebot
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
+keyboard1 = telebot.types.RepleyKeyboardMarkup(True, True)
+keyboard1.row('Привет', 'Пока')
 
 
 @bot.message_handler(commands=['start'])
@@ -8,10 +10,6 @@ def send_welcome(message):
     bot.reply_to(message,
                  f'Я бот-гид по Солнечной системе. Приятно познакомиться, {message.from_user.first_name}, и да '
                  f'прибудет с тобой сила!')
-
-
-keyboard1 = telebot.types.RepleyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Пока')
 
 
 @bot.message_handler(content_types=['text'])
