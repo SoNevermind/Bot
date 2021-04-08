43e32cc762cdfc7c1ed77245e23604990f03e539
Nevermind <1276411@bsu.edu.ru>
2021-04-08 14:51:12 +0300

    ,

--- src/main/main.py
+++ src/main/main.py
@@ -2,6 +2,7 @@ import telebot
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
 
+<<<<<<< Updated upstream
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -9,13 +10,18 @@ def send_welcome(message):
                  f'Я бот-гид по Солнечной системе. Приятно познакомиться, {message.from_user.first_name}, и да '
                  f'прибудет с тобой сила!')
 
+=======
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Здравствуй путник! Ты написал мне /start')
+>>>>>>> Stashed changes
 
-@bot.message_hendler(content_types=['text'])
-def get_text_messages(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
-    else:
-        bot.send_message(message.from_user.id, 'Извини, но это я не понимаю')
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Привет, это гид по Солнечной системе!')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай!')
 
 
 bot.polling()
