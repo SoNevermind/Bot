00219f6d23edb11337b0259ba2db357c36e9cd5c
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 16:02:44 +0300

    fuuyhuyhyh

--- src/main/main.py
+++ src/main/main.py
@@ -1,6 +1,8 @@
 import telebot
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Привет', 'Пока')
 
 
 @bot.message_handler(commands=['start'])
