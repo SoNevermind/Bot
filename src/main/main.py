17a5758bdf7c79f889f4756121e82efc8d959e41
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 14:45:14 +0300

    fix

--- src/main/main.py
+++ src/main/main.py
@@ -1,10 +1,15 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, f'Я бот-гид по Солнечной системе. Приятно познакомиться, {message.from_user.first_name}, и да прибудет с тобой сила!')
+    bot.reply_to(message,
+                 f'Я бот-гид по Солнечной системе. Приятно познакомиться, {message.from_user.first_name}, и да '
+                 f'прибудет с тобой сила!')
+
 
 @bot.message_hendler(content_types=['text'])
 def get_text_messages(message):
@@ -13,5 +18,5 @@ def get_text_messages(message):
     else:
         bot.send_message(message.from_user.id, 'Извини, но это я не понимаю')
 
-bot.polling(non_stope=True)
 
+bot.polling(non_stop=True, intarval=0)
