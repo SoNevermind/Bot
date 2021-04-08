c325fe484b2f7bcef28f790ee718b4db15dc8cb3
Fayleek-lol <72887964+Fayleek-lol@users.noreply.github.com>
2021-04-08 15:45:18 +0300

    аыапп

--- src/main/main.py
+++ src/main/main.py
@@ -1,8 +1,6 @@
 import telebot
 
 bot = telebot.TeleBot('1756217731:AAFJZYYU9GrQezzgSrHCHOdZ1hvW0Cg5Zoo')
-keyboard1 = telebot.types.RepleyKeyboardMarkup(True, True)
-keyboard1.row('Привет', 'Пока')
 
 
 @bot.message_handler(commands=['start'])
@@ -14,10 +12,10 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай')
+    if message.text == 'Привет':
+        bot.send_message(message.chat.id, 'Привет, вот что я могу!')
+    elif message.text == 'Пока':
+        bot.send_message(message.chat.id, 'Прощай!')
 
 
 bot.polling(none_stop=True, interval=0)
